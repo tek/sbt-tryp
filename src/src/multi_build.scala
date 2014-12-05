1db135e46da16fdd96b5ad6b0fec0f467ccc74fe
tek <torstenschmits@gmail.com>
2014-12-05 06:16:19 +0100

    remove MultiBuild.defaults

--- src/src/multi_build.scala
+++ src/src/multi_build.scala
@@ -8,12 +8,6 @@ abstract class MultiBuild(deps: Deps, proguard: Proguard) extends Build {
   val platform: String
 
   lazy val platformSetting = (platformTarget in Android := platform)
-  lazy val notransitiveSetting = (transitiveAndroidLibs in Android := false)
-
-  lazy val defaults = Seq(
-    notransitiveSetting,
-    platformSetting
-    )
 
   def p(name: String) = new ProjectParameters(name, deps, proguard,
     platformSetting)
