c2a53db7137ce87c51742299144a6f999b07af42
tek <torstenschmits@gmail.com>
2015-05-04 12:11:57 +0200

    set scala version and language options by default

--- core/src/multi_build.scala
+++ core/src/multi_build.scala
@@ -1,17 +1,31 @@
 package tryp
 
-import sbt.Setting
+import sbt._
+import Keys._
 
 object DefaultDeps extends Deps
 
 abstract class MultiBuildBase(deps: Deps = DefaultDeps)
 extends sbt.Build
 {
+  override lazy val settings = super.settings ++ Seq(
+    scalaVersion := "2.11.6",
+    scalacOptions ++= Seq(
+      "-feature",
+      "-deprecation",
+      "-language:implicitConversions",
+      "-language:postfixOps",
+      "-language:reflectiveCalls",
+      "-language:experimental.macros"
+    )
+  )
+
   def globalSettings: List[Setting[_]] = Nil
 }
 
 class MultiBuild(deps: Deps = DefaultDeps)
 extends MultiBuildBase
 {
-  def p(name: String) = new DefaultProjectBuilder(name, deps, globalSettings: _*)
+  def p(name: String) =
+    new DefaultProjectBuilder(name, deps, globalSettings: _*)
 }
