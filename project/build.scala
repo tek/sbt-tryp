8556b1c62be536a89d1c8169a4efe191ba1d68dd
tek <torstenschmits@gmail.com>
2015-06-21 20:54:45 +0200

    publish to bintray using sbt-release

--- project/build.scala
+++ project/build.scala
@@ -1,16 +1,15 @@
 import sbt._
 import sbt.Keys._
 
+import bintray._
+import BintrayKeys._
+
 object TrypBuild extends sbt.Build
 {
-  val snapshot = sys.props.getOrElse("TRYP_SBT_RELEASE", "0") != "1"
-
   val aVersion = "1.3.24"
-  val trypVersion = if (snapshot) "1.3-SNAPSHOT" else aVersion
 
   override lazy val settings = super.settings ++ Seq(
     name := "tryp-plugin",
-    version := trypVersion,
     organization := "tryp.sbt"
   )
 
@@ -18,17 +17,9 @@ object TrypBuild extends sbt.Build
     sbtPlugin := true,
     scalaSource in Compile <<= baseDirectory(_ / "src"),
     licenses += ("MIT", url("http://opensource.org/licenses/MIT")),
-    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
-    publishTo := {
-      val nexusUri = sys.props.getOrElse("NEXUS_HOST",
-        default = "http://localhost:8081")
-      val repos = s"$nexusUri/nexus/content/repositories/"
-      val tpe = if (snapshot) "snapshots" else "releases"
-      Some(tpe at repos + tpe)
-    },
-    publishArtifact in (Compile, packageDoc) := false,
-    publishArtifact in (Compile, packageSrc) := false
-  )
+    bintrayRepository in bintray := "sbt-plugins",
+    bintrayOrganization in bintray := None
+  ) ++ BintrayPlugin.bintrayPublishSettings
 
   lazy val core = (project in file("core"))
     .settings(common: _*)
