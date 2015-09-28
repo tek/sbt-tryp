b9523189f6f0d5f95bfd09710652e66caa2be384
tek <torstenschmits@gmail.com>
2015-09-28 02:10:20 +0200

    add protify

--- project/build.scala
+++ project/build.scala
@@ -31,17 +31,16 @@ object TrypBuild extends sbt.Build
   )
 
   lazy val core = (project in file("core"))
+    .settings(common: _*)
     .settings(
-      (name := "tryp-build") ::
-      addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.13.0") ::
-      addSbtPlugin("com.earldouglas" % "xsbt-web-plugin" % "2.0.1") ::
-      addSbtPlugin("me.lessis" % "bintray-sbt" % "0.3.0") ::
-      (libraryDependencies +=
-        "org.scalamacros" % "quasiquotes" % "2.+" cross CrossVersion.binary) ::
-      addCompilerPlugin("org.scalamacros" % "paradise" % "2.+" cross
-        CrossVersion.full) ::
-      common
-      : _*
+      (name := "tryp-build"),
+      addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.13.0"),
+      addSbtPlugin("com.earldouglas" % "xsbt-web-plugin" % "2.0.1"),
+      addSbtPlugin("me.lessis" % "bintray-sbt" % "0.3.0"),
+      libraryDependencies +=
+        "org.scalamacros" % "quasiquotes" % "2.+" cross CrossVersion.binary,
+      addCompilerPlugin(
+        "org.scalamacros" % "paradise" % "2.+" cross CrossVersion.full)
     )
 
   lazy val android = (project in file("android"))
